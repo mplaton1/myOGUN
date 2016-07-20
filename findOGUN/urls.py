a143bfd900b81cd34a4dd9970ccc779c5bc05109
filip <plata12@wp.pl>
2016-07-20 15:49:17 +0200

    added simple displaying

--- findOGUN/urls.py
+++ findOGUN/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^browse', views.browse, name='browse'),
     url(r'', views.search, name='search'),
 ]
--- findOGUN/views.py
+++ findOGUN/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from findOGUN.models import Ogun
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+def browse(request):
+    oguns_list = Ogun.objects.all().filter(ogun_group_id="HUM")
+    paginator = Paginator(oguns_list, 20)
+
+    page = request.GET.get('page')
+    try:
+        oguns = paginator.page(page)
+    except PageNotAnInteger:
+        oguns = paginator.page(1)
+    except EmptyPage:
+        oguns = paginator.page(paginator.num_pages)
+
+    context = { 'oguns' : oguns }
+    return render(request, 'findOGUN/index.html', context)
 
 
 def search(request):
-    return render(request, 'findOGUN/index.html', {})
+    context = {}
+    return render(request, 'findOGUN/index.html', context)
